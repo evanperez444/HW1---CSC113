517c6028a45668df93e9ecc3535d72728776778e
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2023-06-12 17:30:55 -0400

    Update eperez_hw1.py

--- eperez_hw1.py
+++ eperez_hw1.py
@@ -3,27 +3,32 @@
 #Python Programming
 #Assignment 1
 
-
+#1
 name = "John Smith"
 age = 25
 is_student = True
 
+#2
 print('Name:' + name)
 print('Age:' + str(age))
 print('is_student: ' + str(is_student))
 
+#3, 4
 curr_year = 2023
 birth_year = curr_year - age
 print('Birth Year: ' + str(birth_year))
 
+#5, 6
 bio = name + ' is ' + str(age) + ' years old and is a student '
 print(bio)
 
+#7, 8, 9 , 10
 shopping_list = ['Fruit', 'Juice', 'Cheese']
 print(shopping_list)
 shopping_list.append('Chocolate')
 print(shopping_list)
 
+#11, 12, 13, 14, 15
 student_info = { 'Name' :'John Smith', 'Age' : 25, 'is_student' : True }
 print(student_info)
 print(student_info['Name'])
