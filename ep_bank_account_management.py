c07391969e730d50e9f4836bbbab68b49b676ad4
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2023-06-26 15:53:22 -0400

    Create ep_bank_account_management.py

---
+++ ep_bank_account_management.py
@@ -0,0 +1,29 @@
+class Bank:
+    bank_name = ""
+    def set_bank_name(string):
+        bank_name = string
+
+class Account:
+        ?
+
+
+class SavingsAccount:
+    account_num = 0
+    account_holder = ""
+    account_balance = 0
+    interest_rate = 0
+
+    def balance_inquiry(?):
+
+    def withdrawal(?):
+
+class CheckingAccount:
+    account_num = 0
+    account_holder = ""
+    account_balance = 0
+
+    def deposit(?):
+
+    def withdrawal(?):
+
+    def balance_inquiry(?):
