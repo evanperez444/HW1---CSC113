d77654529868894b58a256bf9b967f32aaa5cfea
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2023-06-27 01:17:14 -0400

    classes

---
+++ checking_account.py
@@ -0,0 +1,10 @@
+class CheckingAccount:
+    account_num = 0
+    account_holder = ""
+    account_balance = 0
+
+    def deposit(?):
+
+    def withdrawal(?):
+
+    def balance_inquiry(?):
---
+++ savings_account.py
@@ -0,0 +1,9 @@
+class SavingsAccount:
+    account_num = 0
+    account_holder = ""
+    account_balance = 0
+    interest_rate = 0
+
+    def balance_inquiry(?):
+
+    def withdrawal(?):
