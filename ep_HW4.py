c57dbe2dab21e4135b3fd7b8074d6229c503ef3e
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2023-07-05 15:14:20 -0400

    new

--- ep_HW4.py
+++ ep_HW4.py
@@ -1,10 +1,11 @@
 #File Reading Project
 #By Evan Perez
 #ID: 24212505
-
+import os
 
 
 def read_file(input_file):
+    handle_exception(input_file)
 
     with open(input_file, 'r') as this_file:
 
@@ -19,6 +20,7 @@ def read_file(input_file):
 
 
 def append_file(input_file, string):
+    handle_exception(input_file)
 
     with open(input_file, mode="r+") as this_file:
 
@@ -29,9 +31,30 @@ def append_file(input_file, string):
     this_file.close()
 
 
+def copy_file(input_file):
+    handle_exception(input_file)
+
+    base = os.path.splitext(input_file)
+
+    output_file = os.path.join(str(base[0]) + '_copy.txt')
+
+    with open(input_file, 'r') as this_file, open(output_file, 'w') as other_file:
+
+        for l in this_file:
+            other_file.write(l)
+
+    this_file.close()
 
 
+def delete_file(input_file):
+    handle_exception(input_file)
+    os.remove(input_file)
 
-append_file('/Users/evanperez/Documents/GitHub/HW1---CSC113/data.txt', 'TESTING WRITING')
 
-read_file('/Users/evanperez/Documents/GitHub/HW1---CSC113/data.txt')
+def handle_exception(input_file):
+    path = input_file
+    if os.path.exists(path) == False:
+        print("File not found")
+        exit()
+
+
