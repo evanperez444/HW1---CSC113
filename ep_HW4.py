ec9965046a1bfcc8f6fa702ef22ee639799ff056
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2023-07-05 14:15:58 -0400

    new

--- ep_HW4.py
+++ ep_HW4.py
@@ -6,17 +6,32 @@
 
 def read_file(input_file):
 
-    file = open(input_file, 'r')
+    with open(input_file, 'r') as this_file:
 
-    current_line = file.readline()
+        current_line = this_file.readline()
 
-    while(current_line != ""):
-        print(current_line)
-        current_line = file.readline()
+        while(current_line != ""):
+            print(current_line)
+            current_line = this_file.readline()
 
-    file.close()
+    this_file.close()
 
 
+
+def append_file(input_file, string):
+
+    with open(input_file, mode="r+") as this_file:
+
+        this_file.read()
+
+        print(string, file=this_file)
+
+    this_file.close()
+
+
+
+
+
+append_file('/Users/evanperez/Documents/GitHub/HW1---CSC113/data.txt', 'TESTING WRITING')
 
 read_file('/Users/evanperez/Documents/GitHub/HW1---CSC113/data.txt')
-
