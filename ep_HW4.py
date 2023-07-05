4f1ba9ef3c6e967b12cf03b716127195478800ff
evanperez444 <129793777+evanperez444@users.noreply.github.com>
2023-07-05 13:59:45 -0400

    Update ep_HW4.py

--- ep_HW4.py
+++ ep_HW4.py
@@ -1,2 +1,22 @@
 #File Reading Project
 #By Evan Perez
+#ID: 24212505
+
+
+
+def read_file(input_file):
+
+    file = open(input_file, 'r')
+
+    current_line = file.readline()
+
+    while(current_line != ""):
+        print(current_line)
+        current_line = file.readline()
+
+    file.close()
+
+
+
+read_file('/Users/evanperez/Documents/GitHub/HW1---CSC113/data.txt')
+
